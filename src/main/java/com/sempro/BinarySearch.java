2ed119998df1fb84c97d58b66048037082c3858f
jlars22 <jonas.larsen@cardlay.com>
2024-09-02 16:11:47 +0200

    ¯\_(ツ)_/¯

--- src/main/java/com/sempro/BinarySearch.java
+++ src/main/java/com/sempro/BinarySearch.java
@@ -1,8 +1,24 @@
 package com.sempro;
 
 public class BinarySearch {
-    public static boolean binarySearch(int[] arr, int value) {
-        return true;
-    }
+    public static boolean binarySearchRecursive(int search, int[] array, int start, int end) {
 
+        int middle = (start + end) / 2;
+
+        if (end < start) {
+            return false;
+        }
+
+
+        if (search < array[middle]) {
+            return binarySearchRecursive(search, array, start, middle - 1);
+        }
+
+        if (search > array[middle]) {
+            return binarySearchRecursive(search, array, middle + 1, end);
+        }
+
+        return search == array[middle];
+    }
+    
 }
